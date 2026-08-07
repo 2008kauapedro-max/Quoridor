f9c5cd6a72654f9621f52a161480cd787e1b2c47
Pedro Kauã <2008kauapedro@gmail.com>
2026-08-07 16:09:35 -0300

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   SUPABASE_URL: "https://eyluwsfgrlytyyrwqynz.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_wEMKh7O-1rsAinlPvSnb-w_D0N68pwb"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bHV3c2Zncmx5dHl5cndxeW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMjMwNDYsImV4cCI6MjEwMTY5OTA0Nn0.OnN5nW00F_7iD58CHtcm-lLG1j_7vbV-YiVg96nDJGk"
 };

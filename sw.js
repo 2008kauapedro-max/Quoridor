7c349a97b12052cc4e0b46a4521e136b69bedd73
Pedro Kauã <2008kauapedro@gmail.com>
2026-08-15 19:09:10 +0000

    sw-bump-cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "theRage-arena-v4";
+const CACHE = "rage-v202608151909";
 const CORE = [
   "/", "/index.html", "/manifest.webmanifest", "/icons/icon.svg",
   "/css/theme.css", "/css/main.css", "/css/components.css", "/css/animations.css",
